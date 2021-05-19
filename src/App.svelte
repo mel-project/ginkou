9f45dbdba46e374e9b16040233f699b881fcdf1d
Jay Butera <buterajay@protonmail.com>
2021-05-19 15:16:49 -0400

    faucet/send work-in-progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,54 @@
 <script>
     import Select, { Option } from '@smui/select';
     import Button from '@smui/button';
+    import Textfield from '@smui/textfield';
     import { onMount } from 'svelte';
 
     const walletd_addr = 'http://127.0.0.1:12345';
 
     export let name;
+    // Current network being used
     let using_net = 0;
+    // Current wallet being used
     let using_wallet = [];
     // wallet name to info
     let wallets = {};
     // Network name to integer id
     let networks = {"Main" : 0, "Test" : 1};
+    // Amount to send in a tx
+    let send_amount = 0;
+
+    async function send_mel(wallet_name, mel) {
+        const micromel = mel * 1000;
+        const wallet   = wallets[wallet_name];
+        const outputs  = [micromel, wallet.total_micromel - micromel];
+        const addr     = walletd_addr + '/wallets/' + wallet_name;
+
+        // Prepare tx
+        const res = await fetch(addr + '/prepare-tx');
+        const tx = await res.json();
+
+        // Send tx
+        await fetch(addr + '/send-tx', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: tx,
+        });
+    }
+
+    // Send faucet to given wallet. Returns a succesful request to walletd,
+    // not a succesful transaction.
+    async function tap_faucet(wallet_name) {
+        let addr = walletd_addr + '/wallets/' + wallet_name + '/send-faucet';
+        await fetch(addr);
+    }
 
     onMount(async () => {
         const res = await fetch(walletd_addr + '/wallets');
         wallets = await res.json();
+        console.log(wallets);
     });
 </script>
 
@@ -32,9 +65,22 @@
     {#each Object.entries(wallets)
             .filter(x => x[1].network == using_net)
             .map(x => x[0]) as wallet}
-        <Option>{wallet}</Option>
+        <Option value={wallet}>{wallet}</Option>
     {/each}
     </Select>
+
+    <!-- show faucet tx if using test net -->
+    {#if using_net == 1}
+        <Button on:click={() => tap_faucet(using_wallet)}>Tap Faucet</Button>
+    {/if}
+
+    <p>{JSON.stringify(wallets[using_wallet])}</p>
+
+    <Textfield bind:value={send_amount}
+        label="Amount"
+        type="number"
+        suffix="mel" />
+    <!-- <Button on:click={() => send_mel()}>Send</Button> -->
 </main>
 
 <style>
