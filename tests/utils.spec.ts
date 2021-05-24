6c7afa8444b37c04110c4929a5803e45ec6e6526
Jay Butera <buterajay@protonmail.com>
2021-05-24 14:20:59 -0400

    Fix mocha ts loading

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,16 +1,8 @@
 import * as chai from 'chai';
 import * as utils from '../src/utils';
-//const { tap_faucet, send_mel, confirm_tx, new_wallet } = require('./utils.ts');
-//const mocha = require('mocha');
 
-/*
-describe('MelWalletd', () => {
-    describe('list wallets', () => {
-        it('should return empty list', () => {
-            true
-        });
+describe('list wallets', () => {
+    it('should return empty list', () => {
+        true
     });
 });
-
-modules.exports = {};
-*/
