259c082db6d8ef74a264f037d9d5ef4e29ad01c8
Jay Butera <buterajay@protonmail.com>
2021-05-24 22:10:15 -0400

    Save progress on tests

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,13 +1,37 @@
 import { assert } from 'chai';
 import * as utils from '../src/utils';
-import type { Wallet } from '../src/utils';
+import type { Wallet, PrivateKey } from '../src/utils';
 import { EitherAsync } from 'purify-ts/EitherAsync';
 
 describe('list wallets', () => {
-    it('should return empty list', () => {
-        EitherAsync( async ({ fromPromise }) => {
+    it('should return empty list', async () => {
+        await EitherAsync( async ({ fromPromise }) => {
             const wallets: Wallet[] = await fromPromise( utils.list_wallets() );
             assert.equal(wallets, []);
-        }).run();
+        })
+        .run();
+    });
+});
+describe('new wallet', () => {
+    it('create a wallet', async () => {
+        /*
+        await EitherAsync( async ({ fromPromise }) => {
+            const wallet_name = 'test';
+            const use_testnet = true;
+
+            const pk: PrivateKey = await fromPromise( utils.new_wallet(wallet_name, use_testnet) );
+        })
+        .run();
+        */
+        const wallet_name = 'test';
+        const use_testnet = true;
+        const res = await utils.new_wallet(wallet_name, use_testnet)
+            //.map( pk => assert(typeof(pk) === 'string') )
+            //.map( pk => assert(false) )
+            //.mapLeft( e => assert(false))
+            .run();
+
+        res.ifLeft( e => { console.log(e); assert(false) });
+        //assert.equal(res.ifLeft(
     });
 });
