126120f4612eb2bfa51c493e3c95197efdd9dbd3
Jay Butera <buterajay@protonmail.com>
2021-06-02 14:26:31 -0500

    Lots of styling and wallet menu

---
+++ src/WalletMenu.svelte
@@ -0,0 +1,17 @@
+<script lang="typescript">
+    import Menu from '@smui/menu';
+    import List, { Item, Text } from '@smui/list';
+
+    export let wallet_names: string[];
+    export let active_wallet: string;
+</script>
+
+<Menu static>
+    <List>
+        {#each wallet_names as wlt}
+            <Item on:SMUI:action={() => (active_wallet = wlt)}>
+                <Text>{wlt}</Text>
+            </Item>
+        {/each}
+    </List>
+</Menu>
