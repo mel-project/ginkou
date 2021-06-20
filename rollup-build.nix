76775d65562837685a09a30b9e5a3f8370123d4f
Jay Butera <buterajay@protonmail.com>
2021-06-20 10:13:49 -0500

    Add nix files generated by node2nix

---
+++ rollup-build.nix
@@ -0,0 +1,18 @@
+{ stdenv, pkgs, nodejs }:
+
+let
+  nodeDependencies = (pkgs.callPackage ./default.nix {}).shell.nodeDependencies;
+in
+stdenv.mkDerivation {
+  name = "ginkou";
+  src = .;
+  buildInputs = [nodejs];
+  buildPhase = ''
+    ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+    export PATH="${nodeDependencies}/bin:$PATH"
+
+    # Build and copy to drv out
+    npm run build
+    cp -r public/build/ $out/
+  '';
+}
