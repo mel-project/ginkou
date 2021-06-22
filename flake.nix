306c7c11ad5b61bb2acbc9709c8a1e76282a1fa0
Jay Butera <buterajay@protonmail.com>
2021-06-22 09:24:44 -0500

    Add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Ginkou wallet javascript frontend"
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils
+  }:
+  flake-utils.lib.defaultSystems (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      ginkou-pkg = pkgs.callPackage ./rollup-build.nix {
+        nodejs = pkgs.nodejs-12_x;
+        rollup = pkgs.nodePackages.rollup;
+      };
+    in
+      {
+        defaultPackage = ginkou-pkg;
+      };
+}
