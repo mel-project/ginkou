2e2664d85eac5f9ec2f30a9d14e062f79059685e
Shane Mendez <mendezsoftware@gmail.com>
2021-08-01 10:31:13 -0600

    basic modal

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,19 +1,18 @@
 <script type="text/javascript">
 
 
-    let _class = "";
-    export {_class as class};
     const close = () => {}
 
 </script>
     
 <template lang="pug">
-    div(class!="container {_class}")
-        slot(close!="{close}")
+    div(class="modal")
+        .content
+            slot(close!="{close}")
 </template>
 
 <style lang="scss">
-    .container{
+    .modal{
         position: absolute;
         z-index: 100;
         background-color: #44444444;
@@ -22,7 +21,9 @@
         display: grid;
         align-content: center;
         justify-content: center;
-        &>* {
+        &>.content {
+            width: 80vw;
+            height:80vh;
             position: relative;
 
         }
