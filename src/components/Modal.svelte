2ca8f460fd505451d696fac89a9b6b2c6fa6b2ea
Shane Mendez <mendezsoftware@gmail.com>
2021-08-01 01:11:53 -0600

    created plan

---
+++ src/components/Modal.svelte
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+
+
+    let _class = "";
+    export {_class as class};
+    const close = () => {}
+
+</script>
+    
+<template lang="pug">
+    div(class!="container {_class}")
+        slot(close!="{close}")
+</template>
+
+<style lang="scss">
+    .container{
+        position: absolute;
+        z-index: 100;
+        background-color: #44444444;
+        width: 100vw;
+        height: 100vh;
+        display: grid;
+        align-content: center;
+        justify-content: center;
+        &>* {
+            position: relative;
+
+        }
+    }
+</style>
+    
