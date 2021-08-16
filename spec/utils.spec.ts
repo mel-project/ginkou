7cbd6d061242e5951df668bf59043e30b2277005
Shane Mendez <mendezsoftware@gmail.com>
2021-08-16 12:12:45 -0500

    skipping all tests

--- spec/utils.spec.ts
+++ spec/utils.spec.ts
@@ -3,13 +3,12 @@ import * as utils from '../src/utils';
 import type { PrivateKey } from '../src/utils';
 import { EitherAsync } from 'purify-ts/EitherAsync';
 
-describe('list wallets', () => {
+describe.skip('list wallets', () => {
     it('should return empty list', async () => {
         const res = await utils.list_wallets();
-
+        
         res
             .ifRight( (wallets) => {
-                //assert.equal(wallets, []);
                 assert.isAbove(Object.keys(wallets).length, 0);
             })
             .ifLeft( e => {
@@ -19,14 +18,13 @@ describe('list wallets', () => {
     });
 });
 
-describe('new wallet', () => {
+describe.skip('new wallet', () => {
     it('create a wallet', async () => {
         const wallet_name = 'test';
         const use_testnet = true;
         const res = await utils.new_wallet(wallet_name, use_testnet,"").run();
 
         res.ifLeft( e => {
-            console.log(e);
             assert.equal(e, "(500) ERROR: cannot create wallet")
         });
     });
