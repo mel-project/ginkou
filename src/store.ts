5885a979dd544866e9137648c89eb547afe8514f
Shane Mendez <mendezsoftware@gmail.com>
2022-02-08 13:10:21 -0600

    idk2

--- src/store.ts
+++ src/store.ts
@@ -3,26 +3,15 @@ import { list_wallets, WalletSummary, WalletDump, wallet_dump } from "./utils";
 import JSONbig from "json-bigint";
 
 
-<<<<<<< Updated upstream
-type Obj<T> = { [key: string]: T }
-export interface PersistentSetting {
-=======
 export type Obj<T> = { [key: string]: T }
 export interface PersistentSetting extends String {
->>>>>>> Stashed changes
   default?: any;
 }
 
 export interface Setting extends PersistentSetting {
-<<<<<<< Updated upstream
-  label?: string;
-  type?: string;
-  options?: Obj<string | number>;
-=======
   label: string; // the name of this field
   type: string; // the type of the setting input (can be anything supported by )
   options?: Obj<string | number>; // for types with multiple selection options
->>>>>>> Stashed changes
   depends?: Obj<string | number | boolean>;
   visible?: boolean;
   override?: boolean;
@@ -43,25 +32,6 @@ interface SettingsObject {
   writable_settings: Writable<Settings<string>>;
   set_setting: (name: string, value: string) => void;
 }
-<<<<<<< Updated upstream
-const initSettings = (writable_settings: Readable<Settings<string>>): Settings<Readable<string>> => {
-
-  // subscribe to changes in writable_settings and alter this readable from within
-  function watchSetting(field_name: string) {
-    return (set: any) => {
-      writable_settings.subscribe(($setting) => {
-        // if store_value: readables[name] = $store_value[name]
-        // what is readables?
-        // readables is a mapping from writable_settings: {string: string} => {string: Readable<string>} 
-        if ($setting) {
-          set($setting[field_name])
-        }
-      })
-    }
-  }
-
-  let read_only_settings: Obj<Readable<string>> = {};
-=======
 const get_persistent_settings = (localName: string): Obj<any> =>  {
   const persistent_settings = localStorage.getItem(localName)
   if (persistent_settings)
@@ -83,7 +53,6 @@ const initSettings = (writable_settings: Readable<Settings<string>>): Settings<W
  
 
   let settings: Obj<Writable<string>> = {};
->>>>>>> Stashed changes
   // create inital map from Object<string> to Object<Readable<String>>
   // call immediately after to unsubscribe
   const do_once_and_unsubscribe = writable_settings.subscribe(($settings) => {
@@ -103,15 +72,10 @@ const initSettings = (writable_settings: Readable<Settings<string>>): Settings<W
 // settings 
 export const Settings = (setting_types: Settings<Setting>): SettingsObject => {
   const writable_settings: Writable<Settings<string>> = writable({}, (set) => {
-<<<<<<< Updated upstream
-
-    const persistent_settings: Obj<any> = get_persistent_settings("writable_settings");
-=======
     // start settings as the previous saved state
     const settings: Obj<any> = get_persistent_settings("settings");
     // for each setting, check if setting.override is set or if it didn't exists in persistent storage
     // in either case, use it's default value
->>>>>>> Stashed changes
     Object.entries(setting_types).forEach((entry: [string, Setting]) => {
       const setting_name: string = entry[0];
       const setting: Setting = entry[1];
@@ -121,17 +85,11 @@ export const Settings = (setting_types: Settings<Setting>): SettingsObject => {
     set(persistent_settings)
   }) as unknown as Writable<Settings<string>>; // guarenteed unless setting_types is improperly cast 
 
-<<<<<<< Updated upstream
-  writable_settings.subscribe((value) => {
-    localStorage.setItem("writable_settings", JSONbig.stringify(value))
-  })
-=======
   // // whenever writable settings is changed
   // writable_settings.subscribe((value) => {
   //   // resave the entire thing to local storage
   //   localStorage.setItem("writable_settings", JSONbig.stringify(value))
   // })
->>>>>>> Stashed changes
 
   let set_setting: SettingsObject["set_setting"] = (name, value) => { };
 
