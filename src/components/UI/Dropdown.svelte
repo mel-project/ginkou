726a5c7e7c9c448c49dfc1c08b1e9ded79df32c0
Shane Mendez <mendezsoftware@gmail.com>
2022-02-18 18:35:17 -0600

    still some jank but it works... I hope

--- src/components/UI/Dropdown.svelte
+++ src/components/UI/Dropdown.svelte
@@ -5,15 +5,17 @@
   export let items = [];
   export let stringify;
   export let hovered;
-
+  export let hovered_item;
+  export let active = false;
+  export let disabled = false;
   // edge case:
   // hovered element is destroyed never calling `mouseleave` 
   // hovered == item, even when the item is gone
   $: {
     // on changes to items, make sure the hovered item still exists
-    if(!items.includes(hovered)){
-      hovered = undefined
-    }
+    // if(!items.includes(hovered)){
+    //   hovered = undefined
+    // }
   }
   let _class;
   export {_class as class}
@@ -24,12 +26,15 @@
 </script>
 
 <template lang="pug">
-    div(class!="menu {_class}" tabindex="-1")
-      +each("items as item")
-        div.item(
-          on:mouseenter!="{()=>{hovered=item}}"
-          on:mouseleave!="{()=>{hovered=undefined}}"
-        on:click!="{()=>event_dispatcher('click', {item})}") {stringify(item)}
+    +if('!disabled && (active || hovered)')
+      div(class!="menu {_class}" tabindex="-1" 
+          on:mouseenter!="{()=>{hovered=true}}"
+          on:mouseleave!="{()=>{hovered=false}}")
+        +each("items as item")
+          div.item(
+            on:mouseenter!="{()=>{hovered_item=item}}"
+            on:mouseleave!="{()=>{hovered_item=undefined}}"
+          on:click!="{()=>event_dispatcher('click', {item})}") {stringify(item)}
 </template>
 
 <style lang="scss">
