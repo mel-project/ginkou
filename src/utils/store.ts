e97f25d9c401c1006c9222a1e916f6fb4350530c
Shane Mendez <mendezsoftware@gmail.com>
2022-03-04 00:37:35 -0600

    ready to rumble with no smui

---
+++ src/utils/store.ts
@@ -0,0 +1,168 @@
+import { derived, readable, Readable, Subscriber, Writable, writable } from "svelte/store";
+import { list_wallets,  wallet_dump } from "./utils";
+import JSONbig from "json-bigint";
+import { Maybe, Just, Nothing } from "purify-ts/Maybe";
+import type { PersistentValue, PersistentStorage, SettingConfig,
+   Obj, State, StateObject,WalletSummary, WalletDump,Transaction } from "./types";
+
+
+const default_state_obj = (): State<string>=>{
+  return {
+    network: "",
+    persistent_tabs: "",
+    default_tab: "",
+    current_wallet: "",
+    active_tab: "",
+    contacts: "",
+  }
+}
+function assert_object_fields<T extends Object> (default_object: T ) {
+  return (test_object: T): Maybe<T> => {
+    // console.log("test_object", test_object)
+    const property_test = (acc: Boolean, obj: [string, Object]) => acc && test_object.hasOwnProperty(obj[0])
+    if(Object.entries(default_object).reduce(property_test, true)) return Just(test_object)
+    return Nothing
+  }
+}
+
+// for every entry in an object, map a function onto it's value and return the reconstructed object
+function object_map<T, F> (obj_entries: [string, T][], func: (name: string, obj: T) => F){
+  console.log(obj_entries)
+  return Object.assign({}, ...obj_entries.map((entry: [string, T])=>({[entry[0]]:func(entry[0], entry[1])})))
+}
+
+const get_saved_setting = (localName: string): Maybe<PersistentValue> => {
+  let saved_value: string | null = localStorage.getItem(localName)
+  let maybe_value: Maybe<PersistentValue> = saved_value ? Just(Object.assign({},JSONbig.parse(saved_value))) : Nothing
+  // console.log(maybe_value.extract(), "maybed")
+  return maybe_value.chain(assert_object_fields({name: "", value:""}))
+  
+  
+}
+
+const get_all_saved_settings = (storage_name: string, default_settings: PersistentStorage): PersistentStorage=> {
+  return default_settings.map((obj: PersistentValue)=> {
+    return get_saved_setting(storage_name+obj.name)
+  })
+  .filter((m: Maybe<PersistentValue>)=>m.isJust())
+  .map((m:Maybe<PersistentValue>)=>m.extract()) as unknown as PersistentStorage
+
+}
+
+
+
+const config_to_persistent = (name: string, setting_config: SettingConfig): PersistentValue =>  {
+  return {name, value: setting_config.default}
+}
+
+const persistent_to_object = (pv: PersistentValue): Obj<string> => {
+  return {[pv.name]: pv.value}
+}
+const persistent_to_writable = (storage_name: string, pv: PersistentValue): Writable<any> => {
+  let w = writable(pv.value)
+  w.subscribe( (value: any)=>{
+    pv.value = value
+    localStorage.setItem(storage_name+pv.name, JSONbig.stringify(pv))
+  })
+  return w
+}
+
+// const storage_to_state_writable = (storage_name: string, storage: PersistentStorage): State<Writable<any>> => {
+
+//   return object_map(storage.map(persistent_to_object).map(Object.entries), (name: string, obj: PersistentValue)=>persistent_to_writable(storage_name, obj))
+// }
+
+// const derive_state_object = (state: State<Writable<any>>) => {
+//   return derived({}, (set)=>{
+
+//   })
+// }
+const restore_all_settings = (storage_name: string, setting_types: State<SettingConfig>): State<Writable<any>>=>{
+
+    const assert_state = assert_object_fields(default_state_obj())(setting_types as unknown as State<string>) // the type isn't important here, what's important is gettings around typescript to solve the issue of json being too flexible 
+    if(!assert_state) console.warn("store.ts::restore_all_settings\nsettings_types doesn't satisfy type State<T>, be sure all fields in state are satisfied")
+    // this is what the storage state will look like on first start
+    const default_storage: PersistentStorage = Object.entries(setting_types).map((entry: [string, SettingConfig])=>config_to_persistent(entry[0], entry[1]));
+
+    // the previous saved settings state
+    const saved_storage: PersistentValue[] = get_all_saved_settings(storage_name, default_storage);
+
+
+    const default_state: State<PersistentValue> =  Object.assign({}, ...default_storage.map((pv:PersistentValue)=>({[pv.name]:pv})))
+    const saved_state: State<PersistentValue> =  Object.assign({}, ...saved_storage.map((pv:PersistentValue)=>({[pv.name]:pv})))
+    const reconstructed_state: State<PersistentValue> = Object.assign(default_state, saved_state)
+
+    const state: State<Writable<any>> =  Object.assign({}, ...Object.entries(reconstructed_state).map((entry: [string, PersistentValue])=>({[entry[0]]: persistent_to_writable(storage_name,entry[1])})))
+
+    return state
+
+}
+// settings 
+export const AppState = (setting_types: State<SettingConfig>): StateObject => {
+  return { settings: restore_all_settings("ginkou_storage_", setting_types) };
+}
+
+export const Melwalletd = (settings: State<Readable<string>>) => {
+  // Current wallet dump. Automatically talks to the daemon.
+  const current_wallet_dump: Readable<WalletDump> = derived(
+    settings.current_wallet,
+    ($name, set: (a0: any) => void) => {
+      if ($name == null) {
+        set(null);
+      } else {
+        // we set up a timeout that fetches and then dumps stuff properly
+        const refresh = async () => {
+          // fetch the stuff and set
+          const dump = await wallet_dump($name as string);
+          dump
+            .ifLeft((e) => console.error(`error encountered in wallet_dump: ${e}`))
+            .map((dump) => {
+              // console.log("obtained wallet_dump");
+              // console.log(dump);
+              set(dump);
+            });
+        };
+        // const interval = setInterval(refresh, 5000);
+        refresh();
+        return () => clearInterval(0);
+      }
+    },
+    null
+  );
+  // List of all wallets, both mainnet and testnet
+  const wallet_summaries: Readable<Obj<WalletSummary>> = readable({}, (set) => {
+    const refresh = async () => {
+      // fetch the stuff and set
+      const list = await list_wallets();
+      list
+        .ifLeft((e) => console.log(`error encountered in list_wallets: ${JSON.stringify(e)}`))
+        .map((list) => {
+          // console.info("obtained list_wallets");
+          set(list);
+        });
+    };
+    // const interval = setInterval(refresh, 1000);
+    refresh();
+    return () => clearInterval(0);
+
+  });
+  const sorted_confirmed_txx: Readable<
+  [string, [Transaction, number]][] | null
+> = derived(current_wallet_dump, ($dump) => {
+  if ($dump) {
+    let txx = Object.entries(($dump as WalletDump).full.tx_confirmed);
+    txx.sort((a, b) => a[1][1] - b[1][1]);
+    txx.reverse();
+    // console.log(txx);
+    return txx;
+  } else {
+    return null;
+  }
+
+});
+  return { wallet_summaries, current_wallet_dump, sorted_confirmed_txx };
+}
+
+
+export default { AppState, Melwalletd }
+// settings.subscribe((v)=>{})
---
+++ src/utils/utils.ts
@@ -0,0 +1,513 @@
+import { EitherAsync } from "purify-ts/EitherAsync";
+import { Either, Left, Right } from "purify-ts/Either";
+import { Maybe, Just, Nothing } from "purify-ts/Maybe";
+import { get_wallet } from "./storage";
+import { derive_key, decrypt } from "./crypto";
+import JSONbig from "json-bigint";
+import fetch from "cross-fetch";
+import BigNumber from "bignumber.js";
+import type { WalletDump, TxHistory, TxHash, Transaction, CoinID, CoinData, PrivateKey, WalletSummary, WalletEntry } from "./types";
+
+
+console.log('in utils', Either)
+export const MEL = "6d";
+export const TESTNET = 1;
+export const MAINNET = 255;
+const home_addr = "http://127.0.0.1";
+const default_port = 11773;
+
+
+export const kind2str = (kind: number) => {
+  if (kind === 0x00) {
+    return "Normal";
+  } else if (kind === 0x10) {
+    return "Stake";
+  } else if (kind === 0x50) {
+    return "DoscMint";
+  } else if (kind === 0x51) {
+    return "Swap";
+  } else if (kind === 0x52) {
+    return "LiqDeposit";
+  } else if (kind === 0x53) {
+    return "LiqWithdrawal";
+  } else if (kind === 0xff) {
+    return "Faucet";
+  } else {
+    throw "this should never happen";
+  }
+};
+
+export const denom2str = (denom: string) => {
+  if (denom === "6d") {
+    return "µMEL";
+  } else if (denom === "64") {
+    return "µnomDOSC";
+  } else if (denom === "73") {
+    return "µSYM";
+  } else {
+    return "other (" + denom + ")";
+  }
+};
+
+
+export const wallet_dump_default: WalletDump = {
+  summary: {
+    total_micromel: new BigNumber(0),
+    network: 1,
+    address: "",
+    locked: true,
+  },
+  full: {
+    unspent_coins: [],
+    spent_coins: [],
+    tx_in_progress: {},
+    tx_confirmed: {},
+    my_covenant: "",
+    network: 1,
+  },
+};
+
+// Custom type guards
+// ----
+
+function intoTxHistory(x: any): Maybe<TxHistory> {
+  if ("tx_in_progress" in x && "tx_confirmed" in x) {
+    return intoListOf<[TxHash, Transaction]>(x.tx_in_progress, intoTxTuple)
+      .chain((_) =>
+        intoListOf<[TxHash, [Transaction, number]]>(
+          x.tx_confirmed,
+          intoCdhTuple
+        )
+      )
+      .chain((_) => Just(x));
+  } else return Nothing;
+}
+
+function intoTxTuple(x: any): Maybe<[TxHash, Transaction]> {
+  if (typeof x == "object") {
+    return intoTxHash(x[0])
+      .chain((_) => intoTransaction(x[1]))
+      .chain((_) => Just(x));
+  } else return Nothing;
+}
+
+function intoCdhTuple(x: any): Maybe<[TxHash, [Transaction, number]]> {
+  if (typeof x == "object" && typeof x[1][1] == "number") {
+    return intoTxHash(x[0])
+      .chain((_) => intoTransaction(x[1][0]))
+      .chain((_) => Just(x));
+  } else return Nothing;
+}
+
+function intoTransaction(x: any): Maybe<Transaction> {
+  console.log("here",x)
+  if (
+    "kind" in x &&
+    typeof x.kind == "number" &&
+    "inputs" in x &&
+    "outputs" in x &&
+    "fee" in x &&
+    typeof x.fee == "number" &&
+    "data" in x &&
+    typeof x.data == "string" &&
+    "sigs" in x
+  ) {
+    return intoListOf(x.inputs, intoCoinID)
+      .chain((_) => intoListOf(x.outputs, intoCoinData))
+      .chain((_) => intoListOf<string>(x.scripts, intoString))
+      .chain((_) => intoListOf<string>(x.sigs, intoString))
+      .chain((_) => Just(x));
+  } else return Nothing;
+}
+
+function intoCoinID(x: any): Maybe<CoinID> {
+  if ("txhash" in x && "index" in x && typeof x.index == "number") {
+    return intoTxHash(x.txhash).chain((_) => Just(x));
+  } else return Nothing;
+}
+
+function intoCoinData(x: any): Maybe<CoinData> {
+  if (
+    "covhash" in x &&
+    typeof x.covhash == "string" &&
+    "value" in x &&
+    typeof x.value == "number" &&
+    "denom" in x &&
+    typeof x.denom == "string" &&
+    "additional_data" in x &&
+    typeof x.additional_data == "string"
+  )
+    return Just(x);
+  else return Nothing;
+}
+
+function intoPrivateKey(x: any): Maybe<PrivateKey> {
+  return typeof x == "string" ? Just(x) : Nothing;
+}
+
+function intoWallet(x: any): Maybe<WalletSummary> {
+  if (
+    "total_micromel" in x &&
+    typeof x.total_micromel == "number" &&
+    "network" in x &&
+    typeof x.network == "number" &&
+    "address" in x &&
+    typeof x.address == "string"
+  )
+    return Just(x);
+  else return Nothing;
+}
+
+function intoTxHash(x: any): Maybe<TxHash> {
+  if (typeof x == "string")
+    // && x.length() == 256)
+    return Just(x);
+  else return Nothing;
+}
+
+function intoString(x: any): Maybe<string> {
+  return Just(x);
+}
+
+// Helpers
+
+export async function get_priv_key(
+  active_wallet: string,
+  password: string
+): Promise<ArrayBuffer> {
+  const data = get_wallet(active_wallet);
+  const key = await derive_key(password, data.salt);
+  return decrypt(data.priv_key, key, data.iv);
+}
+
+// Return x in Just x or throw error on Nothing.
+// Convenient do-notation for maybe types.
+/*
+function do_maybe<T>(e: Maybe<T>): T {
+    if ( e.isJust() )
+        return e.extract();
+    else
+        throw e.extract();
+}
+*/
+
+// Do notation for EitherAsync
+/*
+async function do_ea<L,R>(ea: EitherAsync<L,R>): Promise<R> {
+    let e = await ea;
+    return do_either(e);
+}
+function do_either<L,R>(e: Either<L,R>): R {
+    if ( e.isRight() )
+        return e.extract();
+    else
+        throw e.extract();
+}
+*/
+
+// Casting functions
+// ----
+
+/*
+function intoTxHash(x: any): Maybe<TxHash> {
+    return is<TxHash>(x) ? Just(x) : Nothing;
+}
+
+function intoWallet(x: any): Maybe<Wallet> {
+    return is<Wallet>(x) ? Just(x) : Nothing;
+}
+
+function intoPrivateKey(x: any): Maybe<PrivateKey> {
+    return is<PrivateKey>(x) ? Just(x) : Nothing;
+}
+*/
+
+function intoListOf<T>(a: any, intoT: (a0: any) => Maybe<T>): Maybe<T[]> {
+  if (Array.isArray(a))
+    for (const x of a) {
+      if (intoT(x).isNothing()) return Nothing;
+    }
+
+  return Just(a as T[]);
+}
+
+// Give a string cast error if maybe is nothing
+function cast_to_either<T>(m: Maybe<T>): Either<string, T> {
+  return m.caseOf({
+    Just: (x) => Right(x),
+    Nothing: () => Left("failed to cast json to expected type"),
+  });
+}
+
+// Compute total value flowing out of wallet from a list of coins
+export function net_spent(tx: Transaction, self_covhash: string): BigNumber {
+  return tx.outputs
+    .filter((cd) => cd.covhash != self_covhash)
+    .filter((cd) => cd.denom == MEL)
+    .map((cd) => cd.value)
+    .reduce((a, b) => a.plus(b), new BigNumber(0))
+    .plus(tx.fee);
+}
+
+/// Fetch a url endpoint and parse as json, error if the HTTP response is not OK
+export async function fetch_json_or_err(
+  url: string,
+  opts: any
+): Promise<Either<string, any>> {
+  // Throws on a promise rejection, which will be caught by EitherAsync's run()
+  let res = await fetch(url, opts);
+
+  if (!res.ok) return Left("(" + res.status + ") " + (await res.text()));
+  else return Right(JSONbig.parse(await res.text()));
+}
+
+/// Fetch a url endpoint and parse as json, error if the HTTP response is not OK
+export async function fetch_text_or_err(
+  url: string,
+  opts: any
+): Promise<Either<string, string>> {
+  // Throws on a promise rejection, which will be caught by EitherAsync's run()
+  let res = await fetch(url, opts);
+
+  if (!res.ok) return Left("(" + res.status + ") " + (await res.text()));
+  else return Right(await res.text());
+}
+
+// Send faucet to given wallet. Returns a successful request to melwalletd,
+// not a successful transaction.
+export const tap_faucet = (
+  wallet_name: string,
+  port: number = default_port
+): EitherAsync<string, TxHash> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    console.log('tapping')
+    const url = `${home_addr}:${port}/wallets/${wallet_name}/send-faucet`;
+    let res = await fromPromise(fetch_json_or_err(url, { method: "POST" }));
+    return liftEither(cast_to_either(intoTxHash(res)));
+  });
+
+// Creates a new wallet and returns a private key for the new wallet.
+export const new_wallet = (
+  wallet_name: string,
+  use_testnet: boolean,
+  password: string,
+  port: number = default_port
+): EitherAsync<string, void> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const url = `${home_addr}:${port}/wallets/${wallet_name}`;
+    await fromPromise(
+      fetch_text_or_err(url, {
+        method: "PUT",
+        body: JSONbig.stringify({ testnet: use_testnet, password: password }),
+      })
+    );
+
+    return liftEither(Right(undefined));
+  });
+
+// Locks up a wallet.
+export const lock_wallet = (
+  wallet_name: string,
+  port: number = default_port
+): EitherAsync<string, void> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const url = `${home_addr}:${port}/wallets/${wallet_name}/lock`;
+    await fromPromise(
+      fetch_text_or_err(url, {
+        method: "POST",
+      })
+    );
+
+    // Response is a quoted string, so json parses to a string
+    return liftEither(Right(undefined));
+  });
+
+// Unlocks a wallet.
+export const unlock_wallet = (
+  wallet_name: string,
+  password: string,
+  port: number = default_port
+): EitherAsync<string, void> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const url = `${home_addr}:${port}/wallets/${wallet_name}/unlock`;
+    await fromPromise(
+      fetch_text_or_err(url, {
+        method: "POST",
+        body: JSONbig.stringify({
+          password: password,
+        }),
+      })
+    );
+
+    return liftEither(Right(undefined));
+  });
+
+// // Poll daemon to check tx until it is confirmed
+// // TODO handle when daemon returns failed tx
+// export const confirm_tx = async (
+//   url: string,
+//   txhash: TxHash
+// ): Promise<Either<string, BlockHeight>> =>
+//   EitherAsync(async ({ liftEither, fromPromise }) => {
+//     let res: Response = await fromPromise(
+//       fetch_json_or_err(url, { method: "GET" })
+//     );
+//     let json = JSONbig.parse(await res.text());
+//     const height: BlockHeight | null = json.confirmed_height;
+
+//     if (height != null) return liftEither(Right(height as BlockHeight));
+//     // Poll until tx is confirmed
+//     else return await fromPromise(confirm_tx(url, txhash));
+//   });
+
+export const add_coin = (
+  wallet_name: string,
+  coin_id_string: string,
+  port: number = default_port
+): EitherAsync<string, void> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const url_add_coin = `${home_addr}:${port}/wallets/${wallet_name}/coins/${coin_id_string}`;
+    await fromPromise(
+      fetch_text_or_err(url_add_coin, {
+        method: "PUT",
+        body: "",
+      })
+    );
+  });
+
+export const send_tx = (
+  wallet_name: string,
+  transaction: Transaction,
+  port: number = default_port
+): EitherAsync<string, TxHash> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const url_send_tx = `${home_addr}:${port}/wallets/${wallet_name}/send-tx`;
+
+    // Send tx
+    const e_txhash = await fromPromise(
+      fetch_json_or_err(url_send_tx, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(transaction),
+      })
+    );
+
+    // Runtime type check and return
+    return liftEither(cast_to_either(intoTxHash(e_txhash)));
+  });
+
+  export const prepare_mel_tx = (
+    wallet_name: string,
+    to: string[],
+    micromel: BigNumber[],
+    additional_data: string = "",
+    port: number = default_port
+  ): EitherAsync<string, Transaction> =>
+    EitherAsync(async ({ liftEither, fromPromise }) => {
+      const url_prepare_tx = `${home_addr}:${port}/wallets/${wallet_name}/prepare-tx`;
+      
+      if(to.length != micromel.length) 
+        return liftEither(Left("Size of `to` not equal to `micromel`: each recipient must have a matching mel value"))
+  
+      const outputs: CoinData[] =
+        to.map((address, idx)=>
+        ({
+          covhash: address,
+          value: micromel[idx],
+          denom: MEL,
+          additional_data: additional_data,
+        }));
+        
+        console.log(outputs)
+      // Prepare tx (get a json-encoded tx back)
+      const tx: Either<string, any> = await fromPromise(
+        fetch_json_or_err(url_prepare_tx, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSONbig.stringify({
+            outputs: outputs,
+          }),
+        })
+      );
+  
+      // Runtime type check and return
+      console.log(tx)
+      console.log(intoTransaction(tx))
+      return liftEither(cast_to_either(intoTransaction(tx)));
+    });
+  
+
+export const send_mel = (
+  wallet_name: string,
+  wallet: WalletSummary,
+  to: string,
+  mel: BigNumber,
+  additional_data: string = "",
+  port: number = default_port
+): EitherAsync<string, TxHash> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    let tx_res = await prepare_mel_tx(
+      wallet_name,
+      to,
+      mel,
+      additional_data,
+      port
+    );
+
+    return liftEither(cast_to_either(intoTransaction(tx_res)))
+      .chain((tx) => send_tx(wallet_name, tx))
+      .chain((txhash_str) =>
+        liftEither(cast_to_either(intoTxHash(txhash_str)))
+      );
+  });
+
+// Get a list of all stored wallets
+export const list_wallets = (
+  port: number = default_port
+): EitherAsync<string, WalletSummary[]> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const url = `${home_addr}:${port}/wallets`;
+    const res = await fromPromise(fetch_json_or_err(url, { method: "GET" }));
+
+    return liftEither(cast_to_either(intoListOf(res, intoWallet)));
+  });
+
+export const wallet_dump = (
+  wallet_name: string,
+  port: number = default_port
+): EitherAsync<string, WalletDump> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const url = `${home_addr}:${port}/wallets/${wallet_name}`;
+    const res = await fromPromise(fetch_json_or_err(url, { method: "GET" }));
+
+    // TODO cast this with runtime checks
+    return liftEither(Right(res as WalletDump));
+  });
+
+  export const wallet_dump_from_entry = (
+    wallet_entry: WalletEntry,
+    port: number = default_port
+  ): EitherAsync<string, WalletDump> =>
+    EitherAsync(async ({ liftEither, fromPromise }) => {
+      const url = `${home_addr}:${port}/wallets/${wallet_name}`;
+      const res = await fromPromise(fetch_json_or_err(url, { method: "GET" }));
+  
+      // TODO cast this with runtime checks
+      return liftEither(Right(res as WalletDump));
+    });
+
+// Get a TxHistory of a given wallet
+export const tx_history = (
+  wallet_name: string,
+  port: number = default_port
+): EitherAsync<string, TxHistory> =>
+  EitherAsync(async ({ liftEither, fromPromise }) => {
+    const dump = await fromPromise(wallet_dump(wallet_name, port));
+
+    return liftEither(cast_to_either(intoTxHistory(dump.full)));
+  });
+
