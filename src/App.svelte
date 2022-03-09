11b6e369bebc6832a7f593ef4fde38d7bdf801fc
Shane Mendez <mendezsoftware@gmail.com>
2022-03-09 02:04:22 -0600

    dont show every message, just the previous

--- src/App.svelte
+++ src/App.svelte
@@ -1,132 +1,132 @@
 <script lang="ts">
-  import "../node_modules/material-design-lite/material.min.css"
-  import "../node_modules/material-design-lite/material.min.js"
-  import { TESTNET, MAINNET } from "./utils/utils";
-  import {onMount, setContext} from 'svelte'
+import "../node_modules/material-design-lite/material.min.css"
+import "../node_modules/material-design-lite/material.min.js"
+import { TESTNET, MAINNET } from "./utils/utils";
+import {onMount, setContext} from 'svelte'
 
-  import Tab from "./components/UI/tabs/Tab.svelte";
-  import TabBar from "./components/UI/tabs/TabBar.svelte";
-  import Label from "./components/UI/Label.svelte"
-  import Button from "./components/UI/inputs/Button.svelte"
-  import Send from "./views/Send.svelte";
-  import Receive from "./views/Receive.svelte";
-  import Transactions from "./views/Transactions.svelte";
-  import Contacts from "./views/Contacts.svelte";
+import Tab from "./components/UI/tabs/Tab.svelte";
+import TabBar from "./components/UI/tabs/TabBar.svelte";
+import Label from "./components/UI/Label.svelte"
+import Button from "./components/UI/inputs/Button.svelte"
+import Send from "./views/Send.svelte";
+import Receive from "./views/Receive.svelte";
+import Transactions from "./views/Transactions.svelte";
+import Contacts from "./views/Contacts.svelte";
 
-  import SettingsView from "./views/Settings.svelte"
-  import WalletMenu from "./components/WalletMenu.svelte";
-  import { AppState, Melwalletd } from "./utils/store";
-  import Hamburger from "./components/Hamburger.svelte";
-  import TransactionIcon from '@/res/icons/transactions.svg';
-  import SendIcon from '@/res/icons/send.svg';
-  import RecieveIcon from '@/res/icons/recieve.svg';
-  import SettingsIcon from '@/res/icons/settings.svg';
-  import ContactsIcon from '@/res/icons/contacts.svg';
+import SettingsView from "./views/Settings.svelte"
+import WalletMenu from "./components/WalletMenu.svelte";
+import { AppState, Melwalletd } from "./utils/store";
+import Hamburger from "./components/Hamburger.svelte";
+import TransactionIcon from '@/res/icons/transactions.svg';
+import SendIcon from '@/res/icons/send.svg';
+import RecieveIcon from '@/res/icons/recieve.svg';
+import SettingsIcon from '@/res/icons/settings.svg';
+import ContactsIcon from '@/res/icons/contacts.svg';
 
-  import Modal from "./components/UI/windows/Modal.svelte";
-  import type { SettingConfig, State } from "./utils/types";
+import type { SettingConfig, State } from "./utils/types";
 import Dialog from "./components/UI/windows/Dialog.svelte";
 
 
-  const tabs = ["Transactions", "Send", "Receive", "Contacts"]
-  const tab_icons = {"Transactions": TransactionIcon, "Send": SendIcon, "Receive": RecieveIcon,"Contacts": ContactsIcon}
+const tabs = ["Transactions", "Send", "Receive", "Contacts"]
 
-  // change this cuz wtf
-  const tab_components = Object.assign({},...
-    [Transactions, Send, Receive, Contacts].map((comp,i)=>({[tabs[i]]:comp})))
-    
-  const setting_types: State<SettingConfig> = {
-    network: {
-      label: "Network", 
-      field: "select", 
-      options: {Test: TESTNET, Main: MAINNET, All: 0}, 
-      default: TESTNET
-      },
+const tab_icons = {"Transactions": TransactionIcon, "Send": SendIcon, "Receive": RecieveIcon,"Contacts": ContactsIcon}
 
-
-    default_tab: {
-      label: "Default Tab", 
-      field: "select", 
-      options: {Transactions: "Transactions", Send: "Send", Recieve: "Receive"}, 
-      depends: {persistent_tabs: false}, default:"Transactions"},
-
-    persistent_tabs:{ 
-      label: "Persistent Tabs", 
-      field: "checkbox", 
-      visible: true,
-       default: false
-    },
-      current_wallet:{default: "", visible: false},
-      active_tab: {default: "Send", visible: false},
-      contacts: {visible: false, default: []},
-  }
-  // localStorage.clear()
-  const {settings} = AppState(setting_types)
-  const {persistent_tabs, current_wallet, default_tab, active_tab, network} = settings
-  // console.log(persistent_tabs, current_wallet, default_tab, active_tab, $network)
-  // const store = Store(settings)
-
-  // show restraint when using contexts
-  // pass settings as props through components if possible
-  setContext("settings", {settings})
-  setContext("melwalletd", Melwalletd(settings))
-
- 
-
+// change this cuz wtf
+const tab_components = Object.assign({},...
+  [Transactions, Send, Receive, Contacts].map((comp,i)=>({[tabs[i]]:comp})))
   
-  // Indicates whether modal is open: true or closed: false
-  let modal_is_active = false;
-  // Indicates whether the side nav bar is active
-  let wallet_menu_is_active = false;
-  // Indicates whether secret key will be visible
-  // User-viewable error reporting
-  let error_chan: string[] = [];
-  // Channel to notify when a tx has been sent
-  let sent_tx_chan: string[] = [];
-  // Push a message to one of the main notification channels of this UI
-  // (error_chan, sent_tx_chan)
-  function notify_err_event(e: any) {
-    notify_err(e.detail.text);
+const setting_types: State<SettingConfig> = {
+  network: {
+    label: "Network", 
+    field: "select", 
+    options: {Test: TESTNET, Main: MAINNET, All: 0}, 
+    default: TESTNET
+    },
+
+
+  default_tab: {
+    label: "Default Tab", 
+    field: "select", 
+    options: {Transactions: "Transactions", Send: "Send", Recieve: "Receive"}, 
+    depends: {persistent_tabs: false}, default:"Transactions"},
+
+  persistent_tabs:{ 
+    label: "Persistent Tabs", 
+    field: "checkbox", 
+    visible: true,
+      default: false
+  },
+    current_wallet:{default: "", visible: false},
+    active_tab: {default: "Send", visible: false},
+    contacts: {visible: false, default: []},
+}
+// localStorage.clear()
+const {settings} = AppState(setting_types)
+const {persistent_tabs, current_wallet, default_tab, active_tab, network} = settings
+// console.log(persistent_tabs, current_wallet, default_tab, active_tab, $network)
+// const store = Store(settings)
+
+// show restraint when using contexts
+// pass settings as props through components if possible
+setContext("settings", {settings})
+setContext("melwalletd", Melwalletd(settings))
+
+
+
+
+// Indicates whether modal is open: true or closed: false
+let modal_is_active = false;
+// Indicates whether the side nav bar is active
+let wallet_menu_is_active = false;
+// Indicates whether secret key will be visible
+// User-viewable error reporting
+let error_chan: string[] = [];
+// Channel to notify when a tx has been sent
+let sent_tx_chan: string[] = [];
+// Push a message to one of the main notification channels of this UI
+// (error_chan, sent_tx_chan)
+function notify_err_event(e: any) {
+  notify_err(e.detail.text);
+}
+function notify_err(msg: string) {
+  // error_chan = [...error_chan, msg];
+  error_chan = [msg]
+  // Remove message after 5 seconds
+  setTimeout(() => {
+    // lifo queue
+    error_chan = error_chan.slice(1);
+  }, 5000);
+}
+
+function notify_sent_tx_event(e: any) {
+  notify_sent_tx(e.detail.text);
+}
+function notify_sent_tx(msg: string) {
+  sent_tx_chan = [...sent_tx_chan, msg];
+  sent_tx_chan = [msg]
+  // Remove message after 5 seconds
+  setTimeout(() => {
+    // lifo queue
+    sent_tx_chan = sent_tx_chan.slice(1);
+  }, 5000);
+}
+onMount(()=>{
+
+
+  if(!$persistent_tabs){
+    //TODO implement $last_tab setting
+    // should capture the last visited tab to automatically load that tab on startup
+    $active_tab =  $default_tab || "Send"
   }
-  function notify_err(msg: string) {
-    error_chan = [...error_chan, msg];
-
-    // Remove message after 5 seconds
-    setTimeout(() => {
-      // lifo queue
-      error_chan = error_chan.slice(1);
-    }, 5000);
-  }
-
-  function notify_sent_tx_event(e: any) {
-    notify_sent_tx(e.detail.text);
-  }
-  function notify_sent_tx(msg: string) {
-    sent_tx_chan = [...sent_tx_chan, msg];
-
-    // Remove message after 5 seconds
-    setTimeout(() => {
-      // lifo queue
-      sent_tx_chan = sent_tx_chan.slice(1);
-    }, 5000);
-  }
-  onMount(()=>{
-
-
-    if(!$persistent_tabs){
-      //TODO implement $last_tab setting
-      // should capture the last visited tab to automatically load that tab on startup
-      $active_tab =  $default_tab || "Send"
-    }
-    // const {wallet_summaries} = store
-    // console.log($wallet_summaries)
-  })
+  // const {wallet_summaries} = store
+  // console.log($wallet_summaries)
+})
 </script>
 
 <main>
 
   {#if modal_is_active}
-    <Dialog title="Settings V1.0.6" open={modal_is_active} on:close="{()=>{modal_is_active=false}}">
+    <Dialog title="Settings V0.0.7" open={modal_is_active} on:close="{()=>{modal_is_active=false}}">
         <SettingsView 
           {setting_types}
           {settings}
@@ -174,27 +174,28 @@ import Dialog from "./components/UI/windows/Dialog.svelte";
     </div>
     <!--</TopAppBar>-->
   </div>
-
-  <div id="sent-tx-notification-banner">
-    <!-- Report sent-txs to user-->
-    {#if sent_tx_chan.length > 0}
-      <div class="sent-tx-notif-container">
-        {#each sent_tx_chan as msg}
-          <p>{msg}</p>
-        {/each}
-      </div>
-    {/if}
-  </div>
-
-  <div id="error-notification-banner">
-    <!-- Report errors to user-->
-    {#if error_chan.length > 0}
-      <div class="error-notif-container">
-        {#each error_chan as msg}
-          <p>{msg}</p>
-        {/each}
-      </div>
-    {/if}
+  <div class="banners">
+    <div id="sent-tx-notification-banner">
+      <!-- Report sent-txs to user-->
+      {#if sent_tx_chan.length > 0}
+        <div class="sent-tx-notif-container">
+          {#each sent_tx_chan as msg}
+            <p class="banner">{msg}</p>
+          {/each}
+        </div>
+      {/if}
+    </div>
+  
+    <div id="error-notification-banner">
+      <!-- Report errors to user-->
+      {#if error_chan.length > 0}
+        <div class="error-notif-container">
+          {#each error_chan as msg}
+            <p class="banner">{msg}</p>
+          {/each}
+        </div>
+      {/if}
+    </div>
   </div>
 
   <div class="content">
@@ -237,5 +238,20 @@ import Dialog from "./components/UI/windows/Dialog.svelte";
   cursor: pointer;
   fill: white;
   z-index: 100;
+}
+.banners{
+  // position: absolute;
+  // z-index: 99999;
+  // bottom: 0;
+  // text-overflow: ellipsis;
+  // overflow: hidden;
+  // max-width: 100vw;
+}
+.banner{
+//   top: 0;
+// position: relative;
+// left: 0;
+// overflow: hidden;
+// text-overflow: ellipsis;
 }
 </style>
