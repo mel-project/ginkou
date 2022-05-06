0a55c2dad294b65514da5bfc19439cd95ad12b15
nullchinchilla <nullchinchilla@pm.me>
2022-05-06 17:11:25 -0500

    polyfill

--- src/utils/main.ts
+++ src/utils/main.ts
@@ -1,6 +1,7 @@
 import App from "../App.svelte";
 import "../res/styles/app.scss";
 import "toastify-js/src/toastify.css";
+import "@babel/polyfill";
 
 const app = new App({
   target: document.body,
