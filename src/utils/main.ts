ad70f210e459a2aaa014ac17be52e67b877522b3
Shane Mendez <mendezsoftware@gmail.com>
2022-05-17 15:07:11 -0500

    toast works

--- src/utils/main.ts
+++ src/utils/main.ts
@@ -2,10 +2,10 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "core-js/es/set";
 import "core-js/es/weak-map";
-
+import "../res/styles/app.scss";
+import "toastify-js/src/toastify.css";
 import App from "../App.svelte";
 
-import "../res/styles/app.scss";
 
 const app = new App({
   target: document.body,
