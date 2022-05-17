8d787f1ea48a0b5cb5abb9e169519de2e0badee7
Shane Mendez <mendezsoftware@gmail.com>
2022-05-17 13:54:45 -0500

    skipping tests

--- spec/app.spec.ts
+++ spec/app.spec.ts
@@ -1,11 +1,10 @@
-// import App from '../src/App.svelte'
-// import { render, fireEvent } from '@testing-library/svelte'
-// it.skip('Opens the transaction tab', async ()=>{
-//   // const app_element = render(App)
-//   // const transaction_tab = app_element.getByText('Transaction')
+import { render, fireEvent } from '@testing-library/svelte'
+it.skip('Opens the transaction tab', async ()=>{
+  // const app_element = render(App)
+  // const transaction_tab = app_element.getByText('Transaction')
 
-//   // await fireEvent.click(transaction_tab)
+  // await fireEvent.click(transaction_tab)
 
-//   // expect(transaction_tab.getElementsByClassName('transactions'))
+  // expect(transaction_tab.getElementsByClassName('transactions'))
 
-// })
+})
