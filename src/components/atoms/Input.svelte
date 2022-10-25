5cc9dd6a0bdc4afca2169af7cc1c18a1c322ca25
Shane Mendez <mendezsoftware@gmail.com>
2022-10-24 19:07:58 -0500

    still going

---
+++ src/components/atoms/Input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    type ValueTypes = number | bigint | string;
+    type T = $$Generic<ValueTypes>;
+    export let value: T;
+    export let toString: (t:T)=>string;
+    $:  value_to_string = toString(value);
+</script>
+
+<template>
+    <input 
+    {...$$props}
+    type="{typeof value}"
+    value="{value_to_string}"
+    >
+</template>
