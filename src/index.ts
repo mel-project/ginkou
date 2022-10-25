4eb0b55f3a253a8b7649e6d60985140b4e7d8a11
Shane Mendez <mendezsoftware@gmail.com>
2022-10-25 00:26:15 -0500

    added a few things

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+
+export let a = 1;
+
---
+++ src/views/Receive.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<main>
+hello
+</main>
+
+<svelte:head>
+<!-- Fonts -->
+<link
+  rel="res/stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+</svelte:head>
+
+<style lang="scss">
+
+</style>
